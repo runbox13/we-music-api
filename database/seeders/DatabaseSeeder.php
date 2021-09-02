36350107b05799fbce5d58c906933dbe382cd991
peefeeyatko <marshall.anthonys@gmail.com>
2021-09-02 21:10:11 +1000

    user model, migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        $this->createUsers();
+    }
+
+    function createUsers()
+    {
+        DB::table('users')->insert([
+            'email' => 'marshall.anthonys@gmail.com',
+            'password' => Hash::make('password'),
+            'display_name' => 'pfeeyatko',
+            'bio' => 'Lorem ipsum dolor sit amet',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        // dummy users
+        for ($i = 0; $i < 15; $i++) {
+            DB::table('users')->insert([
+                'email' => Str::random(8).'@gmail.com',
+                'password' => Hash::make('password'),
+                'display_name' => Str::random(8),
+                'bio' => 'Lorem ipsum dolor sit amet',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ]);
+        }
     }
 }
