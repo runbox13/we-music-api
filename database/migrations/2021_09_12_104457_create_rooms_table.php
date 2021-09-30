e84e5803d55b3af371fac2d74f1084124ed2bd02
peefeeyatko <marshall.anthonys@gmail.com>
2021-09-30 19:31:10 +1000

    cascade on delete

--- database/migrations/2021_09_12_104457_create_rooms_table.php
+++ database/migrations/2021_09_12_104457_create_rooms_table.php
@@ -17,7 +17,7 @@ class CreateRoomsTable extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable(); 
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
